ac59ccc6411fafeb2695e5934dfe65d3c450a695
Bambaa <poolemedzo@gmail.com>
2026-09-15 12:22:55 +0000

    Update API_URL to point to the deployed script

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Colle ici l'URL de ton Google Apps Script Web App après déploiement.
-const API_URL = "COLLE_ICI_URL_GOOGLE_APPS_SCRIPT";
+const API_URL = "https://script.google.com/macros/s/AKfycbxnrRt6uhIxVrJgX5wSF2Ia4cHSOEcRVLFmt5af_KOE-zZ4CqTaGdvda5Rfeb7a2rEV/exec";
